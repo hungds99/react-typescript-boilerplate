6ff322c864c879234ddd3fee57e5b481aa56c146
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-10-20 10:59:56 +0700

    feat: add UI components including Skeleton, Collapsible, Tooltip, Avatar, Breadcrumb, and Separator; implement mobile detection hook

--- src/components/layout/protected-layout/protected-layout.tsx
+++ src/components/layout/protected-layout/protected-layout.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
 export const ProtectedLayout = () => {
-  return (
-    <div>
-      <h1>Protected route</h1>
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 };
